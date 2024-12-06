446f43489d4c8e55df2baf00b9e3a0067fa6a940
StefanHritcu <shritcu@proton.me>
2024-12-06 21:27:13 +0100

    feat: advaned TailwindCSS brakpoints

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -21,6 +21,37 @@ module.exports = {
         small: clamp("0.75rem", "11.5rem"), // Responsive for small
         base: clamp("1rem", "11.5rem"), // Responsive base size
       },
+            screens: {
+        // Extra small phones and old devices
+        xs: "320px",
+        xs2: "350px",
+
+        // Small smartphones (older iPhones, and small Android models)
+        sm: "375px",
+        sm2: "414px",
+
+        // Medium to large smartphones (current generation devices)
+        md: "480px",
+        md2: "540px",
+
+        // Tablets
+        tablet: "600px",
+        "tablet-lg": "768px",
+        "tablet-xl": "900px",
+
+        // Laptops and medium-sized devices
+        laptop: "1024px",
+        "laptop-md": "1100px",
+        "laptop-lg": "1366px",
+
+        // Desktops and monitors
+        desktop: "1440px",
+        "desktop-lg": "1600px",
+        "desktop-xl": "1920px",
+
+        // Ultra-large devices
+        "ultra-desktop": "2560px",
+      },
     },
   },
   plugins: [],
